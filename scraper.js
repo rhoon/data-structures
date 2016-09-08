e0f4ae60cf431a630aa1776d282f2ebdcb0217df
Riley Hoonan <hoonr188@newschool.edu>
2016-09-08 22:56:33 +0000

    grabs all text within container tr

--- scraper.js
+++ scraper.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 
-var content = fs.readFileSync('/home/ubuntu/workspace/data/syllabus.txt');
+var content = fs.readFileSync('data/data-01.txt');
 
 var $ = cheerio.load(content);
 
 // Print to console: all reading assignments
-$('h4').each(function(i, elem) {
-    if ($(elem).text() == "Read") {
-        $(elem).next().find('li').each(function(i, elem) {
+$('td').each(function(i, elem) {
+    if ($(elem).css("border-bottom") == "1px solid #e3e3e3" && $(elem).css("width") == "260px") {
+        // $(elem).next().find('li').each(function(i, elem) {
             console.log($(elem).text());
-        });
+        // });
     }
 })
