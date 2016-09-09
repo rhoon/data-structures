f68bebda3a4998c44e57d333e7a6e30b3385e79d
Riley Hoonan <hoonr188@newschool.edu>
2016-09-09 00:14:19 +0000

    doing it wrong

--- scraper.js
+++ scraper.js
@@ -5,11 +5,37 @@ var content = fs.readFileSync('data/data-01.txt');
 
 var $ = cheerio.load(content);
 
-// Print to console: all reading assignments
 $('td').each(function(i, elem) {
     if ($(elem).css("border-bottom") == "1px solid #e3e3e3" && $(elem).css("width") == "260px") {
-        // $(elem).next().find('li').each(function(i, elem) {
-            console.log($(elem).text());
-        // });
+
+            var almostAddress = $(elem).text();
+            var textFieldLength = almostAddress.length;
+            var address = '';
+            
+            var isAddress = false;
+            var zipCode = [];
+            
+            for (var j = 0; j < textFieldLength; j++) {
+                
+                // assess if this is the address by seeing if the address has started (it starts with a street number) 
+                // and has not ended (it ends with a zipcode)
+                if (isNumeric(almostAddress[j]) && zipCode.length < 5) {
+                    isAddress = true;
+                    zipCode.push(almostAddress[j]);
+                } 
+                
+                // 
+                // if (isAddress) {
+                // address.push(almostAddress[j]);
+                // }
+                
+                console.log(zipCode);
+            
+            }
+
     }
 })
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
