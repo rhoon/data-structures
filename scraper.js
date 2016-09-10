50e2cea60f26db32f869bf3bfe1fd03461d22d33
Riley Hoonan <hoonr188@newschool.edu>
2016-09-10 01:15:54 +0000

    now returns only address related info + parentheses notes

--- scraper.js
+++ scraper.js
@@ -7,35 +7,9 @@ var $ = cheerio.load(content);
 
 $('td').each(function(i, elem) {
     if ($(elem).css("border-bottom") == "1px solid #e3e3e3" && $(elem).css("width") == "260px") {
-
-            var almostAddress = $(elem).text();
-            var textFieldLength = almostAddress.length;
-            var address = '';
-            
-            var isAddress = false;
-            var zipCode = [];
-            
-            for (var j = 0; j < textFieldLength; j++) {
-                
-                // assess if this is the address by seeing if the address has started (it starts with a street number) 
-                // and has not ended (it ends with a zipcode)
-                if (isNumeric(almostAddress[j]) && zipCode.length < 5) {
-                    isAddress = true;
-                    zipCode.push(almostAddress[j]);
-                } 
-                
-                // 
-                // if (isAddress) {
-                // address.push(almostAddress[j]);
-                // }
-                
-                console.log(zipCode);
-            
-            }
-
+        var a = $(elem).contents().filter(function() {
+                    return this.nodeType == 3;
+                }).text();
+        console.log(a);
     }
 })
-
-function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
