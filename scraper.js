6e178470bd30f07d3119a7e6b31149a643108005
Riley Hoonan <hoonr188@newschool.edu>
2016-09-16 18:34:25 +0000

    updated with more robust code, added nyc and + signs

--- scraper.js
+++ scraper.js
@@ -5,37 +5,53 @@ var content = fs.readFileSync('data/data-01.txt');
 
 var $ = cheerio.load(content);
 
-$('td').each(function(i, elem) {
-    if ($(elem).css("border-bottom") == "1px solid #e3e3e3" && $(elem).css("width") == "260px") {
-        var a = $(elem).contents().filter(function() {
-                    return this.nodeType == 3;
-                }).text();
-                
-        findJunk(a);
-    }
-})
+var addresses = [];
 
-function findJunk(address) {
-    var cutStart;
-    var cutEnd;
+var nyc = ", New York, NY";
+
+$('tbody').find('tr').each(function(i, elem) {
+     addresses
+        .push($(elem)
+        .find('td')
+        .eq(0).html().split('<br>')[2]
+        .split(',')[0].trim()
+        .concat(nyc).replace(/ /g, '+'));
+    })
+
+console.log(addresses);
+
+
+// $('td').each(function(i, elem) {
+//     if ($(elem).css("border-bottom") == "1px solid #e3e3e3" && $(elem).css("width") == "260px") {
+//         var a = $(elem).contents().filter(function() {
+//                     return this.nodeType == 3;
+//                 }).text();
+                
+//         console.log(a);
+//     }
+// })
+
+// function findJunk(address) {
+//     var cutStart;
+//     var cutEnd;
     
-    //locate the parentheses
-    for (var i=0; i<address.length; i++) {
-        if (address[i] == '(') {
-            cutStart = i;
-        } else if (address[i] == ')') {
-            cutEnd = i;
-        }
-    }
+//     //locate the parentheses
+//     for (var i=0; i<address.length; i++) {
+//         if (address[i] == '(') {
+//             cutStart = i;
+//         } else if (address[i] == ')') {
+//             cutEnd = i;
+//         }
+//     }
     
-    //cut 'em on outta there
-    if (cutStart != undefined && cutEnd != undefined) {
-        cut(address, cutStart, cutEnd);
-    } else {
-        console.log(address);
-    }
+//     //cut 'em on outta there
+//     if (cutStart != undefined && cutEnd != undefined) {
+//         cut(address, cutStart, cutEnd);
+//     } else {
+//         console.log(address);
+//     }
     
-}
+// }
 
 //thanks, stack overflow
 function cut(str, cutStart, cutEnd){
